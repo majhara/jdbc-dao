f16def569ca86a92ad9ecc8abc6af09554f575cc
Mayara <may.simoes@hotmail.com>
2023-01-30 19:14:39 -0300

    department insert

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -11,13 +11,21 @@ public class Program2 {
 	public static void main(String[] args) {
 	
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
-		
-		
 			
-		System.out.println("=== TEST 2: Department findById ===");
+		System.out.println("=== TEST 1: Department findById ===");
 		Department dep = departmentDao.findById(1);
 		System.out.print(dep);
+		
+		System.out.println("\n=== TEST 2: Department findAll ===");
+		List<Department> list = departmentDao.findAll();
+		for(Department obj : list) {
+			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST 3: Department insert ===");
+		Department newDep = new Department(null, "Music");
+		departmentDao.insert(newDep);
+		System.out.println("INSERTED! New id: " + newDep.getId());
 	}
 
-
+}
