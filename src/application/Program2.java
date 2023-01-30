e29657dc98e21615464e71fc08d0ccbbd8a2968e
Mayara <may.simoes@hotmail.com>
2023-01-30 19:25:56 -0300

    department update

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -9,23 +9,14 @@ import model.entities.Department;
 public class Program2 {
 
 	public static void main(String[] args) {
-	
+
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
-			
-		System.out.println("=== TEST 1: Department findById ===");
-		Department dep = departmentDao.findById(1);
-		System.out.print(dep);
-		
-		System.out.println("\n=== TEST 2: Department findAll ===");
-		List<Department> list = departmentDao.findAll();
-		for(Department obj : list) {
-			System.out.println(obj);
-		}
-		
-		System.out.println("\n=== TEST 3: Department insert ===");
-		Department newDep = new Department(null, "Music");
-		departmentDao.insert(newDep);
-		System.out.println("INSERTED! New id: " + newDep.getId());
+
+		System.out.println("\n=== TEST 4: Department Update ===");
+		Department dep2 = departmentDao.findById(1);
+		dep2.setName("Food");
+		departmentDao.update(dep2);
+		System.out.println("UPDATED SUCCESSFULLY DONE!");
 	}
 
 }
