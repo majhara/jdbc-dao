e4158a7f0da567a8a749adb83cbc8bf057971224
Mayara <may.simoes@hotmail.com>
2023-01-30 19:02:04 -0300

    department findById, findAll implementations and tests

---
+++ src/application/Program2.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.util.List;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+	
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		
+			
+		System.out.println("=== TEST 2: Department findById ===");
+		Department dep = departmentDao.findById(1);
+		System.out.print(dep);
+		}
+	}
+
+
